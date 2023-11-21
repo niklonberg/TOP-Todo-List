b3b707d7cc67311eeb8ccdd0802d444bd23a05fc
Nikolai Lønberg <niklonberg@gmail.com>
2023-11-21 09:51:56 +0100

    add create to do form function which can be called to dynamically create the todo form and insert it into the page

--- src/modules/FormManager.js
+++ src/modules/FormManager.js
@@ -1,6 +1,8 @@
 /* Handles all form data, submitting and sending that data to other modules*/
 /* gets hold of submitted form data */
 /* can construct an object from form data, which it can send */
+/* import createTodoForm */
+
 const FormManager = (() => {
   let addTodoForm;
 
@@ -8,7 +10,7 @@ const FormManager = (() => {
     addTodoForm = document.querySelector(`#${addTodoFormID}`);
 
     addTodoForm.addEventListener("submit", (event) => {
-      event.preventDefault();
+      event.preventDefault(); /* all of this logic put in seperate function */
       console.log("i worked");
     });
   };
---
+++ src/modules/createTodoForm.js
@@ -0,0 +1,14 @@
+/* function to create and return the todoform, which FormManager can use */
+const createTodoForm = () => {
+  return `
+  <form action="#" id="add-todo-form">
+    <label for="title">Title: </label>
+    <input type="text" name="title" id="title" />
+    <label for="description">Description: </label>
+    <input type="text" name="description" id="description" />
+    <button type="submit">Add todo</button>
+  </form>
+  `;
+};
+
+export default createTodoForm;
