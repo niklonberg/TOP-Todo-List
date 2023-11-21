1ebe0c6d09f3b6b199013794c2c4c77ded20d273
Nikolai Lønberg <niklonberg@gmail.com>
2023-11-21 14:26:04 +0100

    add create project form

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 const log = console.log;
 import FormManager from "./modules/FormManager.js";
 import TodoManager from "./modules/TodoManager.js";
-log(JSON.stringify(TodoManager, null, 2));
-TodoManager.methods.addProject("Vacuum ALan Moore");
-log(JSON.stringify(TodoManager, null, 2));
-TodoManager.methods.deleteProject("Vacuum ALan Moore");
-log(JSON.stringify(TodoManager, null, 2));
 log(TodoManager);
+TodoManager.methods.addProject("Paint Bedroom");
---
+++ src/modules/createProjectForm.js
@@ -0,0 +1,11 @@
+const createProjectForm = () => {
+  return `
+  <form action="#" id="add-todo-form">
+    <label for="title">Title: </label>
+    <input type="text" name="title" id="title" />
+    <button type="submit">Add todo</button>
+  </form>
+  `;
+};
+
+export default createProjectForm;
