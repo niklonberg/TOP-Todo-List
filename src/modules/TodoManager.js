859266b0c15380baee67daec6d38bf36d02c59b3
Nikolai Lønberg <niklonberg@gmail.com>
2023-11-21 12:41:11 +0100

    add to do manager module

---
+++ src/modules/TodoManager.js
@@ -0,0 +1,18 @@
+const TodoManager = (() => {
+  const projects = {
+    //user can add keys that will be new projects
+    default: [
+      //each key is an array of objects, that are each a todo
+      {
+        title: "Wash dishes",
+        description: "Remember to use soap",
+      },
+    ],
+  };
+
+  const addProject = () => {};
+
+  const deleteProject = () => {};
+})();
+
+export default TodoManager;
