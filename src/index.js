f5553950bef8eb0211c488e520798cf1db3c68c4
Nikolai Lønberg <niklonberg@gmail.com>
2023-11-21 11:01:07 +0100

    implemented create to do obj method, which creates an object that TodoFactory takes as input

--- src/index.js
+++ src/index.js
@@ -1,5 +1,2 @@
 const log = console.log;
 import FormManager from "./modules/FormManager.js";
-/* FormManager.initializeAddTodoForm("add-todo-form"); */
-/* FormManager.logForm(); */
-/* log(FormManager.getFormInputs()); */
--- src/modules/FormManager.js
+++ src/modules/FormManager.js
@@ -2,22 +2,37 @@
 /* gets hold of submitted form data */
 /* can construct an object from form data, which it can send */
 import createTodoForm from "./createTodoForm.js";
+import TodoFactory from "./TodoFactory.js";
 
 const FormManager = (() => {
   const createNewTodo = document.querySelector("#create-new-todo"); /* temp */
   const content = document.querySelector("#content");
   let addTodoForm;
 
-  const getFormInputs = () => {
-    return [...addTodoForm.elements].filter((item) => item.tagName === "INPUT");
+  const initializeCreateTodo = () => {
+    addTodoForm = document.querySelector("#add-todo-form");
+    addTodoForm.addEventListener("submit", (event) => {
+      event.preventDefault(); /* make all this into sep. function */
+      console.log(createTodoObj());
+    });
+  };
+
+  const createTodoObj = () => {
+    return [...addTodoForm.elements]
+      .filter((item) => item.tagName === "INPUT")
+      .reduce((resultObj, item) => {
+        resultObj[item.id] = item.value;
+        return resultObj;
+      }, {});
   };
 
   createNewTodo.addEventListener("click", () => {
     content.innerHTML = createTodoForm();
+    initializeCreateTodo();
   });
 
   return {
-    getFormInputs,
+    createTodoObj,
   };
 })();
 
