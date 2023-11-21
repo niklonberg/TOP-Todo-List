c629ac084d6a421afbc9a43dc4b0c91eecaa52a5
Nikolai Lønberg <niklonberg@gmail.com>
2023-11-21 15:23:03 +0100

    add base create new to do button event listener, duplicated for create new project btn

--- src/modules/FormManager.js
+++ src/modules/FormManager.js
@@ -1,13 +1,14 @@
 /* Handles all form data, submitting and sending that data to other modules*/
 /* gets hold of submitted form data */
-/* can construct an object from form data, which it can send */
 import createTodoForm from "./createTodoForm.js";
 import TodoFactory from "./TodoFactory.js";
+import createProjectForm from "./createProjectForm.js";
 import ProjectFactory from "./ProjectFactory.js";
 
 const FormManager = (() => {
-  const createNewTodo = document.querySelector("#create-new-todo"); /* temp */
-  const createNewProject = document.querySelector(
+  const createNewTodoBtn =
+    document.querySelector("#create-new-todo"); /* temp */
+  const createNewProjectBtn = document.querySelector(
     "#create-new-project"
   ); /* temp */
   const content = document.querySelector("#content"); /* temp */
@@ -15,20 +16,31 @@ const FormManager = (() => {
   let addProjectForm;
 
   const initializeForm = (formID) => {
-    form = document.querySelector(`#${formID}`);
+    const form = document.querySelector(`#${formID}`);
     form.addEventListener("submit", (event) => {
       event.preventDefault(); /* make all this into sep. function */
     });
   };
 
+  createNewTodoBtn.addEventListener("click", () => {
+    //atm adds multiple identical eventlisteners if clicked multiple times
+    //make into single function
+    content.innerHTML = createTodoForm();
+    console.log(content);
+    initializeForm(content.id);
+  });
+
+  createNewProjectBtn.addEventListener("click", () => {
+    //atm adds multiple identical eventlisteners if clicked multiple times
+    //make into single function
+    content.innerHTML = createProjectForm();
+    console.log(content);
+    initializeForm(content.id);
+  });
+
   const getInputElements = () => {
     return [...addTodoForm.elements].filter((item) => item.tagName === "INPUT");
   };
-
-  createNewTodo.addEventListener("click", () => {
-    content.innerHTML = createTodoForm();
-    initializeCreateTodo();
-  });
 
   return {
     getInputElements,
