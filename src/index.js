8804310238ba7eb7f32df4340fee55286fb71a27
Nikolai Lønberg <niklonberg@gmail.com>
2023-11-21 10:12:16 +0100

    add reference to create new to do & dialog elements, add html page structure

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const log = console.log;
 import FormManager from "./modules/FormManager.js";
-FormManager.initializeAddTodoForm("add-todo-form");
-FormManager.logForm();
-log(FormManager.getFormInputs());
+/* FormManager.initializeAddTodoForm("add-todo-form"); */
+/* FormManager.logForm(); */
+/* log(FormManager.getFormInputs()); */
--- src/modules/FormManager.js
+++ src/modules/FormManager.js
@@ -1,20 +1,22 @@
 /* Handles all form data, submitting and sending that data to other modules*/
 /* gets hold of submitted form data */
 /* can construct an object from form data, which it can send */
-/* import createTodoForm */
+import createTodoForm from "./createTodoForm.js";
 
 const FormManager = (() => {
+  const createNewTodo = document.querySelector("#create-new-todo"); /* temp */
+  const dialog = document.querySelector("dialog");
   let addTodoForm;
 
-  const initializeAddTodoForm = (addTodoFormID) => {
+  /* const initializeAddTodoForm = (addTodoFormID) => {
     addTodoForm = document.querySelector(`#${addTodoFormID}`);
 
     addTodoForm.addEventListener("submit", (event) => {
-      event.preventDefault(); /* all of this logic put in seperate function */
+      event.preventDefault(); // all of this logic put in seperate function
       console.log("i worked");
     });
   };
-
+ */
   const getFormInputs = () => {
     return [...addTodoForm.elements].filter((item) => item.tagName === "INPUT");
   };
@@ -24,8 +26,13 @@ const FormManager = (() => {
     console.log(addTodoForm.elements);
   };
 
+  createNewTodo.addEventListener("click", () => {
+    dialog.innerHTML = createTodoForm();
+    dialog.showModal();
+  });
+
   return {
-    initializeAddTodoForm,
+    /* initializeAddTodoForm, */
     getFormInputs,
     logForm,
   };
