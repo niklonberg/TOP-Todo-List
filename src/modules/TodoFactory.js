04e416058f1121cdba747a1c9988b9cbf391187b
Nikolai Lønberg <niklonberg@gmail.com>
2023-11-22 09:30:31 +0100

    semantic edit in to do factory

--- src/modules/TodoFactory.js
+++ src/modules/TodoFactory.js
@@ -1,18 +1,19 @@
 function TodoFactory(inputElements) {
-  /* loops through each key in argumentobj */
-  /* returns {} with key:value pairs*/
-  /* title */
-  /* description */
-  /* dueDate */
-  /* priority */
-  /* notes */
-  /* checklist (sub steps) */
-  /* maybe add methods to the objects as well? */
   return inputElements.reduce(
-    (resultObj, item) =>
-      item.value ? { ...resultObj, [item.id]: item.value } : resultObj,
+    (todoObj, item) =>
+      item.value ? { ...todoObj, [item.id]: item.value } : resultObj,
     {}
   );
 }
 
 export default TodoFactory;
+
+/* loops through each key in argumentobj */
+/* returns {} with key:value pairs*/
+/* title */
+/* description */
+/* dueDate */
+/* priority */
+/* notes */
+/* checklist (sub steps) */
+/* maybe add methods to the objects as well? */
