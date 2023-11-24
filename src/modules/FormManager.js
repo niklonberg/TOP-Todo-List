75b2cf5ad61bd9f0d09e0784ffff9f355f50d128
Nikolai Lønberg <niklonberg@gmail.com>
2023-11-24 17:03:25 +0100

    edit event listener on create new project button

--- src/modules/FormManager.js
+++ src/modules/FormManager.js
@@ -1,23 +1,26 @@
-/* Handles all form data, submitting and sending that data to other modules*/
-/* gets hold of submitted form data */
 import createTodoForm from "./createTodoForm.js";
 import createProjectForm from "./createProjectForm.js";
 import ProjectManager from "./ProjectManager.js";
 
 const FormManager = (() => {
-  const createNewTodoBtn =
-    document.querySelector("#create-new-todo"); /* temp */
-  const createNewProjectBtn = document.querySelector(
-    "#create-new-project"
-  ); /* temp */
-  const content = document.querySelector("#content"); /* temp */
-  let addTodoForm; /* needed? */
+  const createNewTodoBtn = document.querySelector("#create-new-todo");
+  const createNewProjectBtn = document.querySelector("#create-new-project");
+  const projectsList = document.querySelector("#projects-list");
+  const mainContent = document.querySelector("#content");
   let addProjectForm; /* needed? */
+  let addTodoForm; /* needed? */
+
+  createNewProjectBtn.addEventListener("click", () => {
+    const form = createProjectForm();
+    projectsList.insertAdjacentHTML("beforeend", form);
+    addProjectForm = document.querySelector("#add-project-form");
+    initializeForm(addProjectForm);
+  });
 
   const initializeForm = (form) => {
     form.addEventListener("submit", (event) => {
       event.preventDefault(); /* make all this into sep. function */
-      ProjectManager.methods.addTodoToSelectedProject(getInputElements(form));
+      ProjectManager.addTodoToSelectedProject(getInputElements(form));
     });
   };
 
@@ -40,11 +43,3 @@ const FormManager = (() => {
 })();
 
 export default FormManager;
-
-/* createNewProjectBtn.addEventListener("click", () => {
-    //atm adds multiple identical eventlisteners if clicked multiple times
-    //make into single function
-    content.innerHTML = createProjectForm();
-    console.log(content);
-    initializeForm(content.id);
-  }); */
