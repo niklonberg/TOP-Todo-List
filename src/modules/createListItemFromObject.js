b8c3ed631d77f0fadc28423ee7239e086720567d
Nikolai Lønberg <niklonberg@gmail.com>
2023-11-29 11:51:58 +0100

    restructure createListItemFromObject

--- src/modules/createListItemFromObject.js
+++ src/modules/createListItemFromObject.js
@@ -1,34 +1,5 @@
 import createElement from "./createElement.js";
 
-function createEditContainer() {
-  const editContainer = createElement("div", "edit-container");
-  const editBtn = createElement("button", "edit-item");
-  editBtn.textContent = "Edit";
-  const deleteBtn = createElement("button", "delete-item");
-  deleteBtn.textContent = "Delete";
-  editContainer.append(editBtn, deleteBtn);
-
-  return editContainer;
-}
-
-function getObjectIDAndTag(object) {
-  const key1 = "projectID";
-  const key2 = "todoID";
-  const objID = object.hasOwnProperty(key1)
-    ? object.projectID
-    : object.hasOwnProperty(key2)
-    ? object.todoID
-    : null;
-
-  const idTag = object.hasOwnProperty(key1)
-    ? "project"
-    : object.hasOwnProperty(key2)
-    ? "todo"
-    : null;
-
-  return [objID, idTag];
-}
-
 function createListItemFromObject(object) {
   const [objID, idTag] = getObjectIDAndTag(object);
 
@@ -68,3 +39,32 @@ function createListItemFromObject(object) {
 }
 
 export default createListItemFromObject;
+
+function createEditContainer() {
+  const editContainer = createElement("div", "edit-container");
+  const editBtn = createElement("button", "edit-item");
+  editBtn.textContent = "Edit";
+  const deleteBtn = createElement("button", "delete-item");
+  deleteBtn.textContent = "Delete";
+  editContainer.append(editBtn, deleteBtn);
+
+  return editContainer;
+}
+
+function getObjectIDAndTag(object) {
+  const key1 = "projectID";
+  const key2 = "todoID";
+  const objID = object.hasOwnProperty(key1)
+    ? object.projectID
+    : object.hasOwnProperty(key2)
+    ? object.todoID
+    : null;
+
+  const idTag = object.hasOwnProperty(key1)
+    ? "project"
+    : object.hasOwnProperty(key2)
+    ? "todo"
+    : null;
+
+  return [objID, idTag];
+}
